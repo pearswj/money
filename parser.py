ce38b4251fc7c1de355bc320f0da1e6c6c082712
Will Pearson <pearswj@gmail.com>
2014-12-29 19:32:54 +0000

    unicode characters are a pain

--- parser.py
+++ parser.py
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
 import re
-#import sys
 
 def tocsv(file):
     """Turns plain-text bank statements into a csv string."""
-    
+
     date = None
     desc = None
     amou = None
@@ -11,17 +11,24 @@ def tocsv(file):
 
     print "date,description,amount,balance"
 
+    SPLIT_CHR = u'\xa0' # '†'
+
     for line in file.readlines():
+        try:
+            line = line.decode('windows-1252') # 'utf-8'
+        except:
+            print 'Error decoding unicode input.'
+            return
         #if line.startswith("From"):
             #print line.strip().split('\xa0')
         if line.startswith("Date"):
-            date = line.strip().split('\xa0')[1]
+            date = line.strip().split(SPLIT_CHR)[1]
         if line.startswith("Description"):
-            desc = re.split(',[0-9]', line.strip().split('\xa0')[1])[0]
+            desc = re.split(',[0-9]', line.strip().split(SPLIT_CHR)[1])[0]
         if line.startswith("Amount"):
-            amou = line.strip().split('\xa0')[1]
+            amou = line.strip().split(SPLIT_CHR)[1].encode('ascii', 'ignore')
         if line.startswith("Balance"):
-            bala = line.strip().split('\xa0')[1]
+            bala = line.strip().split(SPLIT_CHR)[1].encode('ascii', 'ignore')
         if (date and desc and amou and bala):
             print '"%s","%s","%s","%s"' % (date, desc, amou, bala)
             date = None
